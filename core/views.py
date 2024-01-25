801c6d6782314e7122e2920c688f4583ae54f91c
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-01-25 17:21:08 +0530

    chore: Update user agent and language headers for weather app
    Updated the user agent and language headers in the weather app to the latest version. This ensures compatibility with the latest web standards and improves the reliability of web scraping for weather data.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
+
+def get_html_content(city):
+  import requests
+  city = city.replace(' ', '+')
+  USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+  LANGUAGE = "en-US,en;q=0.9"
+  session = requests.Session()
+  session.headers['User-Agent'] = USER_AGENT
+  session.headers['Accept-Language'] = LANGUAGE
+  session.headers['Content-Language'] = LANGUAGE
+  html_content = session.get(f'https://www.google.com/search?q=weather+in+{city}').text
+  return html_content
+
+
 def home(request):
-  return render(request, 'weather.html')
+  weather = None
+  if 'city' in request.GET:
+    # Fetch weather data
+    city = request.GET.get('city')
+    html_content = get_html_content(city)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    weather = dict()
+    weather['region'] = soup.find('div', attrs={'id': 'wob_loc'}).text
+    weather['dayhour'] = soup.find('div', attrs={'id': 'wob_dts'}).text
+    weather['status'] = soup.find('span', attrs={'id': 'wob_dc'}).text
+    weather['temp'] = soup.find('span', attrs={'id': 'wob_tm'}).text
+    print(weather)
+
+  return render(request, 'weather.html', {'weather': weather})
